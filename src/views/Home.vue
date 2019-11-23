91a13a5b58ffb4e9002981c60886c27db76e6b0a
chritoto <32043457+chritoto@users.noreply.github.com>
2019-11-23 10:38:49 -0500

    Devrait marcher

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,8 @@
   <div class="home">
     <HelloWorld msg="Here in my garage!" />
   </div>
-  <div id="app-6">
-    <p>{{ message }}</p>
-    <input v-model="message">
-  </div>
 </template>
 
 <script>
-el: '#app-6';
-  data: {
-    message: 'Hello Vue!';}
+
 </script>
