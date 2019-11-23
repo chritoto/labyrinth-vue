2d94a4476ac788828279f3e92d0d76d8184d0e1c
chritoto <32043457+chritoto@users.noreply.github.com>
2019-11-23 11:50:48 -0500

    hh

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import './firebase'
+import './firebase';
 import Vue from 'vue';
 import './plugins/axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import VueFire from 'vuefire'
+import VueFire from 'vuefire';
 
 Vue.config.productionTip = false;
 Vue.use(VueFire);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,39 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
-    <input v-model="message">
+    <form>
+      <label>
+        New Todo:
+        <input v-model="newTodo" type="text"/>
+        <button type="submit" @click.prevent="addTodo()">Add</button>
+      </label>
+    </form>
   </div>
 </template>
 
 <script>
+import { todosCollection } from '../firebase';
 export default {
   name: 'app',
-  data: {
-    message: 'Hello Vue!'
+  data () {
+    return {
+      newTodo: ''
+    }
+  },
+  methods: {
+    addTodo() {
+      todosCollection.add({
+        text: this.newTodo,
+        completed: false,
+        createdAt: new Date()
+      })
+      .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function(error) {
+        console.error("Error adding document: ", error);
+      });
+      this.newTodo = '';
+    }
   }
-};
+}
 </script>
