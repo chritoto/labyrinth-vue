40ab047f0ad8a11c9812ee82fa3e3ada333d9050
chritoto <32043457+chritoto@users.noreply.github.com>
2019-11-23 11:56:14 -0500

    ujèn

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase';
 
 const app = initializeApp({
-  apiKey: "AIzaSyB8lo0Co4Lx5bLT5Joj8ttScFv3qfbV3ZQ",
-  authDomain: "lessouinceuxdesauce-3808f.firebaseapp.com",
-  databaseURL: "https://lessouinceuxdesauce-3808f.firebaseio.com",
-  projectId: "lessouinceuxdesauce-3808f",
-  storageBucket: "lessouinceuxdesauce-3808f.appspot.com",
-  messagingSenderId: "85801734174",
-  appId: "1:85801734174:web:a7b1ca40610b958e581e0f"
+  apiKey: 'AIzaSyB8lo0Co4Lx5bLT5Joj8ttScFv3qfbV3ZQ',
+  authDomain: 'lessouinceuxdesauce-3808f.firebaseapp.com',
+  databaseURL: 'https://lessouinceuxdesauce-3808f.firebaseio.com',
+  projectId: 'lessouinceuxdesauce-3808f',
+  storageBucket: 'lessouinceuxdesauce-3808f.appspot.com',
+  messagingSenderId: '85801734174',
+  appId: '1:85801734174:web:a7b1ca40610b958e581e0f'
 });
 
 export const db = app.firestore();
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <form>
       <label>
         New Todo:
-        <input v-model="newTodo" type="text"/>
+        <input v-model="newTodo" type="text" />
         <button type="submit" @click.prevent="addTodo()">Add</button>
       </label>
     </form>
@@ -14,26 +14,27 @@
 import { todosCollection } from '../firebase';
 export default {
   name: 'app',
-  data () {
+  data() {
     return {
       newTodo: ''
-    }
+    };
   },
   methods: {
     addTodo() {
-      todosCollection.add({
-        text: this.newTodo,
-        completed: false,
-        createdAt: new Date()
-      })
-      .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-      })
-      .catch(function(error) {
-        console.error("Error adding document: ", error);
-      });
+      todosCollection
+        .add({
+          text: this.newTodo,
+          completed: false,
+          createdAt: new Date()
+        })
+        .then(function(docRef) {
+          console.log('Document written with ID: ', docRef.id);
+        })
+        .catch(function(error) {
+          console.error('Error adding document: ', error);
+        });
       this.newTodo = '';
     }
   }
-}
+};
 </script>
