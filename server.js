3639acbeaa95303209f0e0649fbf01e42250fe87
alexandre marquis-angulo <alexandre.marquis-angulo.1@ulaval.ca>
2019-11-23 10:36:53 -0500

    firetest1

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
+const admin = require('firebase-admin');
+const serviceAccount = require('path/to/serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://lessouinceuxdesauce.firebaseio.com'
+});
 
 const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
