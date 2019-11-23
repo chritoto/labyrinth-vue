6a00ca867162425e6882fcf0080c788c6187b559
chritoto <32043457+chritoto@users.noreply.github.com>
2019-11-23 10:26:07 -0500

    test home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,15 @@
   <div class="home">
     <HelloWorld msg="Here in my garage!" />
   </div>
+  <div id="app-6">
+    <p>{{ message }}</p>
+    <input v-model="message">
+  </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
   }
-};
 </script>
